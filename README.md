65d2969fafbb5f003f588e471b18742cbecd9163
jasondebolt <jasondebolt@gmail.com>
2017-04-14 14:20:16 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef-centos7-httpd-demo
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: chef-centos7-httpd-demo
+# Recipe:: default
+#
+# Copyright (c) 2017 Jason DeBolt, All Rights Reserved.
