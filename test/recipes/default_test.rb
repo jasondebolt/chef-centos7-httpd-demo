8e4beb94fea99737cf741fa776fd6331bdef88e3
jasondebolt <jasondebolt@gmail.com>
2017-04-17 09:45:39 -0700

    commenting out ec2 instance from .kithen.yml file

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -4,16 +4,3 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
-
-remote_file '/var/www/html/jason.jpg' do
-  source 'https://s3-us-west-1.amazonaws.com/jasondeboltpublic/jason_suit_night.jpg'
-  owner 'web_admin'
-  group 'web_admin'
-end
-
-template '/var/www/html/index.html' do
-  source 'index.html.erb'
-  owner 'web_admin'
-  group 'web_admin'
-  mode 00744
-end
